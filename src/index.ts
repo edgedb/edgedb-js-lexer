4b58cc964ec038cd8fe80e3df87443926a586c32
James Clarke <james@edgedb.com>
2020-09-04 13:34:52 +0100

    Add build step to extract generated TokenKind enum

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
-export {TokenizerError} from './error'
+import {TokenizerError, Position} from "./error";
+import {loadLib} from "../loader";
 
-import {loadLib} from "../loader"
+// @ts-ignore
+import {TokenKind} from "./tokens";
 
-export async function lexEdgeQL(str: string): Promise<any> {
+export {TokenizerError, TokenKind};
+
+export interface Token {
+  kind: TokenKind;
+  value: string;
+  position: Position;
+}
+
+export async function lexEdgeQL(str: string): Promise<Token[]> {
   const lib = await loadLib();
   return lib.lexEdgeQL(str);
 }
