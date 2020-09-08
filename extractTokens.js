28594ffdc9329308af7391fe16e5529670d795e2
Yury Selivanov <yury@edgedb.com>
2020-09-08 11:28:22 -0700

    Yet another fix for extractTokens

--- extractTokens.js
+++ extractTokens.js
@@ -3,7 +3,7 @@ const tokens = require("./temp/edgeql_wasm.js").TokenKind;
 console.log(`export enum TokenKind {`);
 
 Object.keys(tokens)
-  .filter((token) => typeof token === "string")
+  .filter((token) => Number.isNaN(parseInt(token, 10)))
   .forEach((token) => console.log(`  ${token} = "${token}",`));
 
 console.log(`}`);
